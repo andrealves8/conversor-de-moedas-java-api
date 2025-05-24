11b41977750baee63ee972a93c54248db1f54cd8
andrealves <andrealves-tec@hotmail.com>
2025-05-23 21:07:33 -0300

    feat: adiciona menu interativo na view para conversão de moedas

--- conversordemoedas/src/conversordemoedas/view/ConversorView.java
+++ conversordemoedas/src/conversordemoedas/view/ConversorView.java
@@ -1,14 +1,7 @@
 package conversordemoedas.view;
 
-/**
- * Classe responsável pela interface com o usuário.
- * Somente exibe informações e lê entradas.
- */
 public class ConversorView {
 
-    /**
-     * Exibe o menu de opções para o usuário.
-     */
     public void exibirMenu() {
         System.out.println("""
             *************************************************
@@ -27,18 +20,10 @@ public class ConversorView {
         """);
     }
 
-    /**
-     * Exibe mensagens genéricas ao usuário.
-     * @param mensagem Texto a ser mostrado.
-     */
     public void exibirMensagem(String mensagem) {
         System.out.println(mensagem);
     }
 
-    /**
-     * Exibe o resultado da conversão.
-     * @param valorFinal Resultado convertido.
-     */
     public void exibirResultado(double valorFinal) {
         System.out.printf("Resultado da conversão: %.6f%n%n", valorFinal);
     }
